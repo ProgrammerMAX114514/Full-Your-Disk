de547595f60f492b048b07461531d1f24965221b
ProgrammerMAX114514 <3276318609@qq.com>
2024-06-14 17:58:42 +0800

    Added user protocol to prevent user misoperation. Added system disk detection (limited to C disks only) to prevent irreversible issues caused by system disk overflow. Because the program cannot run on operating systems other than Windows, a system check has been added to prevent it from running in environments other than Windows.

--- main.py
+++ main.py
@@ -7,6 +7,37 @@ import psutil # 这个模块不在python标准库内，需要自行下载安装�
 4.等待下载完成
 """
 from os import system, listdir
+from random import randint
+
+def warning():
+    text="""用户协议
+警告
+该程序具有类似病毒的功能，在运行前请务必备份好所有重要资料文件。
+如非必要，请勿用该程序填充C盘或系统盘，否则可能造成系统崩溃等突发危险状况。
+总之，该工具仅供学习参考，请勿用于非法用途或改编为非法计算机软件，否则可能面临法律问责！
+请仔细阅读上述文本。"""
+    RED_BOLD = '\033[1;31m'
+    RESET = '\033[0m'
+    print(f"{RED_BOLD}{text}{RESET}")
+    print("若您同意上述内容，请在下方输入：\"我同意上述用户协议，并自愿承担运行后造成的所有后果。\"")
+    print("若不同意，按CTRL+C退出程序。")
+    ans=input("请输入：")
+    if ans == "我同意上述用户协议，并自愿承担运行后造成的所有后果。":
+        print("已同意用户协议。")
+        print("请回答下列问题，以确认你是人类。")
+        a=randint(1,100)
+        b=randint(1,100)
+        correctans=a+b
+        userans=input(f"{a} + {b} = ")
+        if int(userans) == correctans:
+            print("回答正确，继续运行。")
+        else:
+            print("回答错误，程序退出。")
+            exit(-1)
+    else:
+        print("用户协议未同意，程序退出。")
+        exit(-1)
+
 
 # 获取用户操作选择，用于在程序中引导用户进行不同的操作
 def getoperationchoice():
@@ -77,6 +108,7 @@ def getdiskremaining(disk):
 
 # 主程序入口，循环引导用户进行操作
 if __name__ == '__main__':
+    warning()
     while True:
         choice = getoperationchoice()
         performoperation(choice)
